12529862b845959542704ac0b994dcf21766a970
Julian Ahrens <julianahrens@users.noreply.github.com>
2022-07-23 21:15:41 +0200

    make core publishable

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.10"
+    `maven-publish`
 }
 
 group = "io.github.sqlgenerator"
@@ -21,4 +22,39 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "11"
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = group as String?
+            artifactId = rootProject.name
+            version = version
+
+            pom {
+                name.set("SQL-Generator Core")
+                url.set("https://github.com/sql-generator/sql-generator-core")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://raw.githubusercontent.com/sql-generator/sql-generator-core/main/LICENSE")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("julianahrens")
+                        name.set("Julian Ahrens")
+                        email.set("opensource@julianahrens.de")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com:sql-generator/sql-generator-core.git")
+                    developerConnection.set("scm:git:ssh://github.com:sql-generator/sql-generator-core.git")
+                    url.set("https://github.com/sql-generator/sql-generator-core.git")
+                }
+            }
+
+            from(components["java"])
+        }
+    }
 }
